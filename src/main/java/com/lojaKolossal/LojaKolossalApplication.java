849c4f61897e16ecfb37ff062934d71ee1977c1d
leandrochavess <leandrovalqueire@hotmail.com>
2019-01-22 20:01:26 -0200

    meu primeiro commit

---
+++ src/main/java/com/lojaKolossal/LojaKolossalApplication.java
@@ -0,0 +1,14 @@
+package com.lojaKolossal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LojaKolossalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LojaKolossalApplication.class, args);
+	}
+
+}
+
